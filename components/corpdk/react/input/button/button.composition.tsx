6ba4861dd6b729bcaee96e716010a5ba75a591ba
Debraj Kundu <kundudebraj4272@gmail.com>
2023-08-12 20:29:26 +0530

    button component initial design

--- components/corpdk/react/input/button/button.composition.tsx
+++ components/corpdk/react/input/button/button.composition.tsx
@@ -1,8 +1,95 @@
-import React from 'react';
-import { Button } from './button';
+import React from "react";
+import { Button } from "./button";
 
-export const BasicButton = () => {
+export const FilledButton = () => {
   return (
-    <Button>hello world!</Button>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "nowrap",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+      }}
+    >
+      <Button>Filled Button</Button>
+      <Button disabled>Filled Button</Button>
+    </div>
   );
-}
+};
+
+export const OutlinedButton = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "nowrap",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+      }}
+    >
+      <Button variant="outlined">Outlined Button</Button>
+      <Button variant="outlined" disabled>
+        Outlined Button
+      </Button>
+    </div>
+  );
+};
+
+export const TextButton = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "nowrap",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+      }}
+    >
+      <Button variant="text">Text Button</Button>
+      <Button variant="text" disabled>
+        Text Button
+      </Button>
+    </div>
+  );
+};
+
+export const ElevatedButton = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "nowrap",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+      }}
+    >
+      <Button variant="elevated">Elevated Button</Button>
+      <Button variant="elevated" disabled>
+        Elevated Button
+      </Button>
+    </div>
+  );
+};
+
+export const TonalButton = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "nowrap",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+      }}
+    >
+      <Button variant="tonal">Tonal Button</Button>
+      <Button variant="tonal" disabled>
+        Tonal Button
+      </Button>
+    </div>
+  );
+};

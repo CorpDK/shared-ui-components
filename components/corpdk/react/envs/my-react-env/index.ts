4de7a87f5834916bc4f8bdb9cacffbb2da7bfa3b
Debraj Kundu <kundudebraj4272@gmail.com>
2023-08-31 01:20:06 +0530

    env set to react18

--- components/corpdk/react/envs/my-react-env/index.ts
+++
@@ -1,5 +0,0 @@
-import { MyReactEnvAspect } from './my-react-env.aspect';
-
-export type { MyReactEnvMain } from './my-react-env.main.runtime';
-export default MyReactEnvAspect;
-export { MyReactEnvAspect };
--- components/corpdk/react/envs/my-react-env/my-react-env.aspect.ts
+++
@@ -1,5 +0,0 @@
-import { Aspect } from '@teambit/harmony';
-
-export const MyReactEnvAspect = Aspect.create({
-  id: 'corpdk.cdk/envs/my-react-env',
-});
